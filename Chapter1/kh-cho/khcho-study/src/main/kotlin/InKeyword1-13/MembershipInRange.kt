d3fbab05517f45f946300bced0a5577f74935740
조경훈 <jkh991116@naver.com>
2023-12-14 16:08:50 +0900

    ⭐ feat: chapter 1 - 13 in

---
+++ Chapter1/kh-cho/khcho-study/src/main/kotlin/InKeyword1-13/MembershipInRange.kt
@@ -0,0 +1,6 @@
+package Chapter1.`kh-cho`.`khcho-study`.src.main.kotlin.`InKeyword1-13`
+
+fun main() {
+    val percent = 35
+    println(percent in 1..100)
+}
---
+++ Chapter1/kh-cho/khcho-study/src/main/kotlin/InKeyword1-13/MembershipInUsingBounds.kt
@@ -0,0 +1,6 @@
+package Chapter1.`kh-cho`.`khcho-study`.src.main.kotlin.`InKeyword1-13`
+
+fun main() {
+    val percent = 35
+    println(0 <= percent && percent <= 100)
+}
