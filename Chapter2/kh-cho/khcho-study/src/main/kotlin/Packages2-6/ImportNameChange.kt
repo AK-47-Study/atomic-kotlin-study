41482245f5e8c93af9167cee4c0b3f6152bf7147
조경훈 <jkh991116@naver.com>
2023-12-18 19:58:08 +0900

    ⭐ feat: chapter 2 - 6 패키지

---
+++ Chapter2/kh-cho/khcho-study/src/main/kotlin/Packages2-6/ImportNameChange.kt
@@ -0,0 +1,8 @@
+package Chapter2.`kh-cho`.`khcho-study`.src.main.kotlin.`Packages2-6`
+import kotlin.math.PI as circleRatio
+import kotlin.math.cos as cosine
+fun main() {
+    println(circleRatio)
+    println(cosine(circleRatio))
+    println(cosine(2 * circleRatio))
+}
