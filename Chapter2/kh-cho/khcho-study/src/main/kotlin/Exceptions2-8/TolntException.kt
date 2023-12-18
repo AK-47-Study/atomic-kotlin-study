c7c6c164b364a3f11511bae5b89f67d8c3acd6b5
조경훈 <jkh991116@naver.com>
2023-12-18 20:15:36 +0900

    ⭐ feat: chapter 2 - 8 예와

---
+++ Chapter2/kh-cho/khcho-study/src/main/kotlin/Exceptions2-8/TolntException.kt
@@ -0,0 +1,9 @@
+package Chapter2.`kh-cho`.`khcho-study`.src.main.kotlin.`Exceptions2-8`
+
+fun erroneousCode(){
+    val i = "1$".toInt()
+}
+
+fun main() {
+    erroneousCode()
+}
--- Chapter2/kh-cho/khcho-study/src/main/kotlin/Testing2-7/TestingExample.kt
+++ Chapter2/kh-cho/khcho-study/src/main/kotlin/Testing2-7/TestingExample.kt
@@ -1,13 +1,14 @@
-//package Chapter2.`kh-cho`.`khcho-study`.src.main.kotlin.`Testing2-7`
-//
-//import atomictest. *
-//fun main() {
-//    val v1 = 11
-//    val v2 = "Ontology"
-//// R는 ' 같다 ' 는 뜻
-//    v1 eq 11
-//    v2 eq "Ontology"
-//// 'neq'는 ' 같지 않다 ' 는 뜻
-//    v2 neq "Epistimology"
-//// v2 eq "Epistimology"
-//}
+package Chapter2.`kh-cho`.`khcho-study`.src.main.kotlin.`Testing2-7`
+
+import atomictest. *
+fun main() {
+    val v1 = 11
+    val v2 = "Ontology"
+// R는 ' 같다 ' 는 뜻
+    v1 eq 11
+    v2 eq "Ontology"
+// 'neq'는 ' 같지 않다 ' 는 뜻
+    v2 neq "Epistimology"
+// [Error] Epistimology != Ontology
+// v2 eq "Epistimology"
+}
