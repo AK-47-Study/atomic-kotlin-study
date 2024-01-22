f75e9b0df49cdb9fc2e3257aec374a0ee14766ca
oslob99 <jkh991116@naver.com>
2024-01-23 00:03:42 +0900

    ⭐ feat: CHAPTER 7 - 3 재네릭스 만들기

---
+++ Chapter7/kh-cho/khcho-study/src/main/kotlin/creatingGenerics/ReificationA.kt
@@ -0,0 +1,5 @@
+package Chapter7.`kh-cho`.`khcho-study`.src.main.kotlin
+
+fun <T: Any> a(KClass: KClass<T>){
+    // KClass<T>를 사용한다
+}
---
+++ Chapter7/kh-cho/khcho-study/src/main/kotlin/creatingGenerics/ReificationC.kt
@@ -0,0 +1,7 @@
+package Chapter7.`kh-cho`.`khcho-study`.src.main.kotlin
+
+fun <T: Any> a(KClass: KClass<T>)
+
+class K
+
+val kc = c(k::class)
